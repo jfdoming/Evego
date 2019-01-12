6d886163f2c1c0fc2c3b9945d7c35bb3a210afa6
Julian Dominguez-Schatz <dolphinspray.fr@gmail.com>
2019-01-12 18:06:33 -0500

    Added the map component.

--- App.js
+++ App.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import MapCmp from './MapCmp';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello World! :)</Text>
-      </View>
+      <MapCmp></MapCmp>
     );
   }
 }
