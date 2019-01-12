d1f6011ba81cdc8ea9a92c39566bf876eefd3c93
cnnr-adams <cnnr252@gmail.com>
2019-01-12 16:41:05 -0500

    Backend interface

---
+++ backend.js
@@ -0,0 +1,54 @@
+import request from 'request'
+const ip = '34.235.94.25';
+// Usage: import Backend from './backend.js'
+//    getEvents() returns all events as json list
+//    createEvent(latitude,longitude,name,emoji,category) returns event id
+//    goingToEvent(eventID) returns 200 or 404 if event not found
+export default class Backend {
+    getEvents() {
+        return request.get(ip, (error, resonse, body) => {
+            if (error) {
+                console.error(error);
+                return error;
+            }
+            console.log("Got Data:", response.body);
+            return body;
+            if (error) {
+                console.error(error);
+            });
+    }
+    createEvent(latitude, longitude, name, emoji, category) {
+        return request.post(ip + "/event", {
+            json: {
+                "lat": latitude,
+                "long": longitude,
+                "name": name,
+                "emoji": emoji,
+                "category": category
+            }
+        },
+            function (error, response, body) {
+                if (error) {
+                    console.error(error);
+                    return error;
+                }
+                if (!error && response.statusCode == 200) {
+                    return body;
+                }
+            })
+    }
+    goingToEvent(eventID) {
+        return request.post(ip + "/going", {
+            json: {
+                "id": eventID
+            }
+        },
+            function (error, response, body) {
+                if (error) {
+                    console.log(error);
+                    return error;
+                }
+                return body;
+            })
+    }
+}
